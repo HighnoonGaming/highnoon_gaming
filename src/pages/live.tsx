bce270b4472ea2ee7aa7ab027ef5cb3550dd7791
corb0140 <corb0140@algonquinlive.com>
2025-10-29 13:52:37 -0400

    live page completed

--- src/pages/live.tsx
+++ src/pages/live.tsx
@@ -1,5 +1,14 @@
 function Live() {
-  return <div>Live</div>
+  return (
+    <div className="p-10 flex items-center justify-center">
+      <iframe
+        src="https://player.twitch.tv/?channel=highnoongamingbb&parent=localhost"
+        allowFullScreen
+        // width="1920"
+        className="w-full h-145 min-h-120"
+      ></iframe>
+    </div>
+  )
 }
 
 export default Live
