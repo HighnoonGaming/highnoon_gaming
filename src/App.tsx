81316678d12ba94f468784d5fe7ef4081cf74cff
corb0140 <corb0140@algonquinlive.com>
2025-11-06 23:43:57 -0500

    updated env

--- src/App.tsx
+++ src/App.tsx
@@ -113,8 +113,8 @@ function App() {
     moveAll()
   }, [])
 
-  const http =
-    process.env.NODE_ENV === 'production'
+  const base =
+    process.env.BASE_ENV === 'production'
       ? 'https://highnoongaming.vercel.app/'
       : 'localhost'
 
@@ -155,7 +155,7 @@ function App() {
               className="h-screen w-full sticky top-[10vh] z-10 flex flex-col justify-center items-center"
             >
               <iframe
-                src={`https://player.twitch.tv/?channel=highnoongamingbb&parent=${http}`}
+                src={`https://player.twitch.tv/?channel=highnoongamingbb&parent=${base}`}
                 allowFullScreen
                 className="w-full h-145 min-h-120"
               ></iframe>
